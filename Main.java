fb9d99c9f67e0a79ff92d49e891ab61ff96f7a4b
Je Rome <103312494+hashira09p@users.noreply.github.com>
2024-08-16 11:07:18 +0000

    Done Activity

--- Main.java
+++ Main.java
@@ -1,1 +1,21 @@
+/**
+ * Main
+ */
+public class Main {
+    public static void main(String[] args) {
+        int check_number = 10;
+        String message;
 
+        for(int i = 0; i<=check_number; i++){
+            if(i%2==0){
+                message = i + " " + "is Even Number";
+                System.out.println(message);
+            }
+            else{
+                message = i + " " + "is Odd Number";
+                System.out.println(message);
+            }
+        }
+    }
+    
+}
